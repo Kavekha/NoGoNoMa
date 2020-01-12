d6ec7ec8c086f86556eba04b3a10727918761a5c
Kavekha <40246978+Kavekha@users.noreply.github.com>
2020-01-12 16:36:32 +0100

    Delete particule_component.py

--- components/particule_component.py
+++
@@ -1,7 +0,0 @@
-from time import perf_counter
-
-
-class ParticuleLifetimeComponent:
-    def __init__(self, lifetime=0.2):
-        self.lifetime = lifetime
-        self.start_time = perf_counter()
