860d8c2144c3c88abe6812dcb9d58dad73692c7e
Kavekha <40246978+Kavekha@users.noreply.github.com>
2020-01-12 20:32:25 +0100

    Cursed items & unequip fix
           > fix: unequip item
            > Cursed items
            > Impossible to unequip cursed items

--- inventory_system/use_equip_system.py
+++ inventory_system/use_equip_system.py
@@ -3,7 +3,7 @@ from systems.system import System
 from components.intent_components import WantsToUseComponent
 from components.equip_components import EquippableComponent, EquippedComponent
 from components.name_components import NameComponent
-from components.magic_item_components import IdentifiedItemComponent
+from components.magic_item_components import IdentifiedItemComponent, CursedItemComponent
 from components.item_components import InBackPackComponent
 from ui_system.render_functions import get_obfuscate_name
 import config
@@ -27,27 +27,37 @@ class UseEquipSystem(System):
                 item_subjects = World.get_components(EquippedComponent, NameComponent)
                 for item_entity, (already_equipped, named_item) in item_subjects:
                     if already_equipped.owner == entity and already_equipped.slot == entity_slot:
-                        to_unequip.append(item_entity)
-                        if entity == player:
-                            logs.appendleft(f'[color={config.COLOR_SYS_MSG}]'
-                                            f'{Texts.get_text("YOU_UNEQUIP")}'
-                                            f'{Texts.get_text(get_obfuscate_name(item_entity))}[/color]')
+                        cursed = World.get_entity_component(item_entity, CursedItemComponent)
+                        if cursed:
+                            can_equip = False
+                            logs = World.fetch('logs')
+                            logs.appendleft(f'[color={config.COLOR_PLAYER_INFO_NOT}]'
+                                            f'{Texts.get_text("YOU_CANNOT_REMOVE_").format(get_obfuscate_name(item_entity))}'
+                                            f'{Texts.get_text("ITS_CURSED")}[/color]')
+                        else:
+                            to_unequip.append(item_entity)
+                            if entity == player:
+                                logs.appendleft(f'[color={config.COLOR_SYS_MSG}]'
+                                                f'{Texts.get_text("YOU_UNEQUIP")}'
+                                                f'{Texts.get_text(get_obfuscate_name(item_entity))}[/color]')
 
-                for item in to_unequip:
-                    World.remove_component(EquippedComponent, item)
-                    World.add_component(InBackPackComponent(owner=entity), item)
+                if can_equip:
 
-                # else, we equip.
-                World.add_component(EquippedComponent(owner=entity, equipment_slot=entity_slot), use_item.item)
-                World.remove_component(InBackPackComponent, use_item.item)
-                if entity == player:
-                    logs.appendleft(f'[color={config.COLOR_SYS_MSG}]'
-                                    f'{Texts.get_text("YOU_EQUIP")}'
-                                    f'{Texts.get_text(get_obfuscate_name(use_item.item))}'
-                                    f'[/color]')
-                    # identify
-                    item_named = World.get_entity_component(use_item.item, NameComponent)
-                    World.add_component(IdentifiedItemComponent(name=item_named.name), entity)
+                    for item in to_unequip:
+                        World.remove_component(EquippedComponent, item)
+                        World.add_component(InBackPackComponent(owner=entity), item)
+
+                    World.add_component(EquippedComponent(owner=entity, equipment_slot=entity_slot), use_item.item)
+                    World.remove_component(InBackPackComponent, use_item.item)
+                    if entity == player:
+                        logs.appendleft(f'[color={config.COLOR_SYS_MSG}]'
+                                        f'{Texts.get_text("YOU_EQUIP")}'
+                                        f'{Texts.get_text(get_obfuscate_name(use_item.item))}'
+                                        f'[/color]')
+
+                        # identify
+                        item_named = World.get_entity_component(use_item.item, NameComponent)
+                        World.add_component(IdentifiedItemComponent(name=item_named.name), entity)
 
                 remove_use.append(entity)
 
